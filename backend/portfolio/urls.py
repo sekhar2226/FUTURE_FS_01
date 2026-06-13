2739d4054297e03c0a0863e9a9442bed9183cdeb
Sekhar Basava <sekharbasava@gmail.com>
2026-06-13 14:31:42 +0530

    Remove temporary admin route

--- backend/portfolio/urls.py
+++ backend/portfolio/urls.py
@@ -15,11 +15,9 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from contact.views import create_admin
 from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/contact/', include('contact.urls')),
-    path("create-admin/", create_admin),
 ]
